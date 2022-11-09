bbd19cae5345750ce18df0b60af186b25b231129
Enjmateo <enjmateo@yahoo.fr>
2022-11-09 09:43:10 +0100

    First commit

---
+++ src/main/java/fr/insa/hashiwohahero/HashiwohaheroApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.hashiwohahero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashiwohaheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashiwohaheroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/insa/hashiwohahero/HashiwohaheroApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.hashiwohahero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashiwohaheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
